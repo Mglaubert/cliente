d7bec1ee692ece01ded0021e76858976c7b0226a
Moises Glaubert <=>
2023-01-05 18:25:32 -0300

    first commit

---
+++ cliente/src/main/java/br/com/attornatus/cliente/ProjetoAttornatusSpringBootJpaEH2Application.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAttornatusSpringBootJpaEH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAttornatusSpringBootJpaEH2Application.class, args);
+	}
+
+}
---
+++ cliente/src/test/java/br/com/attornatus/cliente/ProjetoAttornatusSpringBootJpaEH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAttornatusSpringBootJpaEH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
